e1dadbf04cb3f2e577eac355c607097aeee27e94
marcos-bah <marcos.barbosa@unifei.edu.br>
2021-08-26 15:11:38 -0300

    [ADD] Adicionando base

---
+++ lib/src/features/maps/viewMap.view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ViewMapView extends StatefulWidget {
+  const ViewMapView({Key? key}) : super(key: key);
+
+  @override
+  _ViewMapViewState createState() => _ViewMapViewState();
+}
+
+class _ViewMapViewState extends State<ViewMapView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(),
+    );
+  }
+}
