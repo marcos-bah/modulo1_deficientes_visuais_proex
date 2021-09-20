87599f7f55301425af2a8bdb27ec3d00dcc7e54c
marcos-bah <marcos.barbosa@unifei.edu.br>
2021-09-20 15:20:54 -0300

    [ADD] Mudando rota inicial

--- lib/src/app/app.widget.dart
+++ lib/src/app/app.widget.dart
@@ -25,7 +25,7 @@ class AppWidget extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.green,
         ),
-        initialRoute: "/view-map",
+        initialRoute: "/",
         routes: {
           "/": (context) => LoginView(),
           "/login": (context) => LoginView(),
