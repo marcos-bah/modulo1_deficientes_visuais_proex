659626daa0c54f2a8f25f949b8753ddb16074843
Daniel Henrique <drandhfc@gmail.com>
2021-09-24 19:21:28 -0300

    [ADD] Visualização via servidor tile do open street map

--- lib/src/features/maps/viewMap.view.dart
+++ lib/src/features/maps/viewMap.view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:modulo1_deficientes_visuais_proex/src/app/app.color.dart';
-import 'package:modulo1_deficientes_visuais_proex/src/features/maps/map.controller.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
+import 'package:latlong2/latlong.dart';
+import 'package:flutter_map/flutter_map.dart';
 
 class ViewMapView extends StatefulWidget {
   const ViewMapView({Key? key}) : super(key: key);
@@ -12,98 +10,32 @@ class ViewMapView extends StatefulWidget {
 }
 
 class _ViewMapViewState extends State<ViewMapView> {
-  MapController controller = MapController();
   @override
-  Widget build(BuildContext context) {
-    final String assetName = 'assets/flutter_logo.svg';
-    final Widget svg = SvgPicture.asset(
-    assetName,
+ Widget build(BuildContext context) {
+  return FlutterMap(
+    options: MapOptions(
+      center: LatLng(-22.41308, -45.45041),
+      zoom: 18,
+    ),
+    layers: [
+      TileLayerOptions(
+        urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        subdomains: ['a', 'b', 'c']
+      ),
+      // MarkerLayerOptions(
+      //   markers: [
+      //     Marker(
+      //       width: 80.0,
+      //       height: 80.0,
+      //       point: LatLng(51.5, -0.09),
+      //       builder: (ctx) =>
+      //       Container(
+      //         child: FlutterLogo(),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+    ],
   );
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Visualizando Mapa"),
-        centerTitle: true,
-        backgroundColor: AppColors.primary,
-      ),
-      body: Center(
-        child: Container(
-          width: size.width < 520 ? size.width * 0.8 : 480,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Container(
-                  width: size.width < 320 ? size.width * 0.8 : 280,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        "Visualização\ndo Mapa",
-                        style: TextStyle(
-                          fontSize: 36,
-                          fontWeight: FontWeight.w700,
-                          color: AppColors.primary,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        "Título:",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                      Text(
-                        controller.getName.isNotEmpty
-                            ? controller.getName
-                            : "----",
-                        style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 14,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        "Descrição:",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                      Text(
-                        controller.getDescription.isNotEmpty
-                            ? controller.getDescription
-                            : "----",
-                        style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 14,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              //Mapa aqui
-              Expanded(
-          child: svg
-        ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+}
 }
